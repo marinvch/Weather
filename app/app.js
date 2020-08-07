9dab147b97367021117f36249cdaf47f5a6c85d4
Marin Cholakov <marinvch@gmail.com>
2020-08-07 05:39:03 +0300

    new Skin

--- app/app.js
+++ app/app.js
@@ -1,10 +1,13 @@
-// app key
 const app = {
     key: '6b1a8f471d1a6dbb35e2eda0fd6cb56f',
     baseUrl: 'api.openweathermap.org/data/2.5',
     reqOptions: {
         method: 'GET',
-        redirect: 'follow'
+        redirect: 'follow',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "text/plain"
+        },
     }
 
 };
@@ -14,21 +17,20 @@ const inputs = {
     btn: document.querySelector('button')
 };
 
-inputs.btn.addEventListener('click', getData);
+inputs.input.addEventListener('keyup', getData);
 
 function getData() {
-
     let city = document.querySelector('input').value
-    fetch(`http://${app.baseUrl}/weather?q=${city}&appid=${app.key}&lang=bg&units=metric`, app.reqOptions)
+
+    fetch(`http://${app.baseUrl}/weather?q=${city}&appid=${app.key}&units=metric`)
         .then(response => response.json())
         .then(result => displayData(result))
         .catch(error => console.log('404 not Found'));
 
-}
-
+};
 
 function displayData(result) {
-    console.log(result)
+
     const info = {
         description: document.getElementById('description'),
         cityname: document.getElementById('city'),
@@ -36,29 +38,29 @@ function displayData(result) {
         wind: document.getElementById('wind')
     };
 
+    let name = result.name
+    let city = document.querySelector('input').value
+    if (city !== '') {
 
 
-    info.description.innerHTML = `${result.weather[0].description}`;
-    info.cityname.innerHTML = `Град: ${result.name}`;
-    info.temperature.innerHTML = `Температура: ${result.main.temp}`;
-    info.wind.innerHTML = `Вятър: ${result.wind.speed}м/с`;
+        info.description.innerHTML = `${result.weather[0].description}`;
+        info.cityname.innerHTML = `Град: ${result.name}`;
+        info.temperature.innerHTML = `Температура: ${result.main.temp}`;
+        info.wind.innerHTML = `Вятър: ${result.wind.speed}м/с`;
+
+    } else {
+        info.description.innerHTML = ''
+        info.cityname.innerHTML = ''
+        info.temperature.innerHTML = ''
+        info.wind.innerHTML = ''
+    }
+
+
 
 };
 
+const ipKey = '2c2637df1b55c4';
 
-const currentPosition = navigator.geolocation.getCurrentPosition((_position) => {
-    let lat = _position.coords.latitude;
-    let long = _position.coords.longitude
-    if (_position) {
-
-        fetch(`http://${app.baseUrl}/weather?lat=${lat}&lon=${long}&appid=${app.key}&lang=bg&units=metric`)
-            .then(response => response.json())
-            .then(result => currentWeather(result))
-            .catch(error => console.log('404 not Found'));
-    } else {
-        alert('Please use your Location to display the data')
-    }
-})
 
 function currentWeather(result) {
     const fields = {
@@ -69,11 +71,50 @@ function currentWeather(result) {
         wind: document.getElementById('currentWind'),
     }
 
-    let url = "http://openweathermap.org/img/w/"
-
-    fields.icon.innerText = "http://openweathermap.org/img/w/" + result.weather[0].icon + ".png",
-        fields.description.textContent = `${result.weather[0].description}`;
     fields.city.textContent = `Град: ${result.name}`;
     fields.temperature.textContent = `Температура: ${result.main.temp}`
     fields.wind.textContent = `Вятър: ${result.wind.speed}м/с`;
+    fields.description.textContent = `${result.weather[0].description}`;
+
 }
+
+
+function do_something(coords) {
+
+    fetch(`http://${app.baseUrl}/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${app.key}&lang=bg&units=metric`)
+        .then(response => response.json())
+        .then(result => currentWeather(result))
+        .catch(error => console.log('404 not Found'));
+
+}
+
+navigator.geolocation.getCurrentPosition((position) => {
+        do_something(position.coords);
+        () => {
+            fetch = ('https://ipinfo.io/geo', (response) => {
+                var loc = response.loc.split(',');
+                var coords = {
+                    latitude: loc[0],
+                    longitude: loc[1]
+                };
+                do_something(coords);
+            });
+        }
+    },
+
+);
+
+const googleApi = 'AIzaSyBu8-exdzeJa1o6zsm3l0Qk6h8tv4tbGQM'
+const crossOrgin = {
+
+}
+
+fetch(`https://www.googleapis.com/geolocation/v1/geolocate?key=${googleApi}`, {
+        method: "GET",
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "text/plain"
+        }
+    })
+    .then(response => response.json())
+    .then(result => console.log(result))
